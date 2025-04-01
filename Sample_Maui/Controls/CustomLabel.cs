edd062cde8c458aa51dc2c73bb1274c8c8eae595
増田直洋 <masu4516@purpose.co.jp>
2025-04-01 15:51:42 +0900

    最低限のカスタムラベル実装

---
+++ Sample_Maui/Controls/CustomLabel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui;
+using System.Collections;
+using Java.Net;
+
+namespace Sample_Maui.Controls
+{
+    public class CustomLabel : Label
+    {/*
+        /// <summary>
+        /// Textプロパティを作成
+        /// </summary>
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+                    propertyName: nameof(Text),  //紐づけるプロパティ名
+                    returnType: typeof(string),  //プロパティの型
+                    declaringType: typeof(CustomLabel),  //このプロパティを持つクラス
+                    defaultValue: default(string)  //デフォルト値
+            );
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        /// <summary>
+        /// TextColorプロパティを作成
+        /// </summary>
+        public static readonly BindableProperty TextColorProperty = BindableProperty.Create(
+                    propertyName: nameof(TextColor),
+                    returnType: typeof(Color),
+                    declaringType: typeof(CustomLabel),
+                    defaultValue: Colors.Black
+            );
+
+        public Color TextColor
+        {
+            get { return (Color)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
+
+        /// <summary>
+        ///FontSizeプロパティを作成
+        /// </summary>
+        public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(
+                    propertyName: nameof(FontSize),
+                    returnType: typeof(double),
+                    declaringType: typeof(CustomLabel),
+                    defaultValue: 10.0
+            );
+
+        public double FontSize
+        {
+            get { return (double)GetValue(FontSizeProperty); }
+            set { SetValue(FontSizeProperty, value); }
+        }
+
+        */
+
+
+    }
+}
---
+++ Sample_Maui/Platforms/Android/Handler/CustomLabelHandler.cs
@@ -0,0 +1,61 @@
+﻿using Sample_Maui.Controls;
+using Microsoft.Maui.Handlers;
+using Android.Widget;
+using Android.App;
+using Android.Util;
+
+
+namespace Sample_Maui.Platforms.Android.Handlers
+{
+    public partial class CustomLabelHandler
+    {
+        //ここは継承しないとエラーになる
+        public static PropertyMapper<CustomLabel, CustomLabelHandler> PropertyMapper = new PropertyMapper<CustomLabel, CustomLabelHandler>(ViewHandler.ViewMapper)
+        {
+
+        };
+
+        //コンストラクタ（base：継承元のコンストラクタを呼び出す）
+        public CustomLabelHandler() : base(PropertyMapper)
+        {
+        }
+    }
+
+    public partial class CustomLabelHandler : ViewHandler<CustomLabel, TextView>
+    {
+        //引数違いのコンストラクタ
+        public CustomLabelHandler(IPropertyMapper mapper, CommandMapper? commandMapper = null) : base(mapper, commandMapper)
+        {
+        }
+
+        //継承にあたり必須のオーバーライド
+        protected override TextView CreatePlatformView()
+        {
+            var textView = new TextView(MainActivity.Instance);
+            // Xamarinのコントロールで設定したFontSizeを取得
+            var size = textView.TextSize;
+            textView.Text = "Comon";
+
+            textView.SetTextSize(ComplexUnitType.Dip, (float)size);
+            return textView;
+        }
+    }
+}
+
+/*
+public class CustomLabelHandler : LabelHandler
+{
+    public CustomLabelHandler(Context context) : base(context)
+    {
+    }
+
+    protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+    {
+        base.OnElementChanged(e);
+
+        // Xamarinのコントロールで設定したFontSizeを取得
+        var size = Element.FontSize;
+        // 単位dpで設定し直す
+        Control.SetTextSize(ComplexUnitType.Dip, (float)size);
+    }
+}*/
